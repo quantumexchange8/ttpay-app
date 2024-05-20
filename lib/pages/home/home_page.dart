5fe141932dee479ba066f5c9d511cf4bd84ad7c4
ZuffyX <hanafirohani.2902@gmail.com>
2024-05-20 17:59:03 +0800

    add notification data from notification controller

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ttpay/controller/controller.dart';
 import 'package:ttpay/helper/dimensions.dart';
-import 'package:ttpay/helper/dummyData/transactions_history.dart';
 import 'package:ttpay/helper/methods.dart';
 import 'package:ttpay/helper/text_style.dart';
 import 'package:ttpay/models/transaction.dart';
@@ -22,8 +23,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String selectedTransactionType = 'All';
   final List<String> tabNameList = ['All', 'Deposit', 'Withdrawal'];
-  final List<Transaction> allTransactions =
-      listTransactionFromListMap(dummyTransactions);
   List<Transaction> currentTransactions = [];
 
   @override
@@ -31,33 +30,8 @@ class _HomePageState extends State<HomePage> {
     const netAmount = 8280828.93;
     const grossAmount = 8973728.37;
     const feeAmount = 8362.38;
-    const unreadNoti = true;
     const totalDepositNumber = 1372;
     const totalFreezingAmount = 8362.38;
-
-    final depositTransactions = allTransactions
-        .where((element) => element.transactionType.toLowerCase() == 'deposit')
-        .toList();
-    final withdrawalTransactions = allTransactions
-        .where(
-            (element) => element.transactionType.toLowerCase() == 'withdrawal')
-        .toList();
-
-    switch (selectedTransactionType) {
-      case 'Deposit':
-        currentTransactions = depositTransactions;
-      case 'Withdrawal':
-        currentTransactions = withdrawalTransactions;
-        break;
-      default:
-        currentTransactions = allTransactions;
-    }
-
-    currentTransactions = currentTransactions
-        .where(
-          (element) => element.createdAt.month == DateTime.now().month,
-        )
-        .toList();
 
     void onTapNoti() {
       Navigator.push(
@@ -73,65 +47,98 @@ class _HomePageState extends State<HomePage> {
       }
     }
 
-    return CustomScrollView(
-      slivers: [
-        homeSliverAppbar(
-          onTapProfile: onTapProfile,
-          onTapNoti: onTapNoti,
-          feeAmount: feeAmount,
-          grossAmount: grossAmount,
-          netAmount: netAmount,
-          unreadNoti: unreadNoti,
-        ),
-        SliverList(
-          delegate: SliverChildListDelegate(
-            [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width08 * 2),
-                child: summartBoxBelowAppBar(
-                    totalDepositNumber: totalDepositNumber,
-                    totalFreezingAmount: totalFreezingAmount),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: width08 * 2, vertical: height08),
-                child: Text(
-                  'Current Month Transactions',
-                  style: textMd.copyWith(fontWeight: FontWeight.w700),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: width08 * 2),
-                child: tabPickerRow(
-                    onTapTab: (tabName) {
-                      setState(() {
-                        selectedTransactionType = tabName;
-                      });
-                    },
-                    tabNameList: tabNameList,
-                    selectedTab: selectedTransactionType),
-              ),
-              SizedBox(
-                height: height08,
-              ),
-              if (currentTransactions.isEmpty)
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: height20),
-                  child: noTransactionsColumn,
-                )
-            ],
+    return Obx(() {
+      final allTransactions = transactionController.transactionList;
+      final notifications = notificationController.notificationList;
+
+      final unreadNoti =
+          notifications.map((element) => element.unread).contains(true);
+
+      final depositTransactions = allTransactions
+          .where(
+              (element) => element.transactionType.toLowerCase() == 'deposit')
+          .toList();
+      final withdrawalTransactions = allTransactions
+          .where((element) =>
+              element.transactionType.toLowerCase() == 'withdrawal')
+          .toList();
+
+      switch (selectedTransactionType) {
+        case 'Deposit':
+          currentTransactions = depositTransactions;
+        case 'Withdrawal':
+          currentTransactions = withdrawalTransactions;
+          break;
+        default:
+          currentTransactions = allTransactions;
+      }
+
+      currentTransactions = currentTransactions
+          .where(
+            (element) => element.createdAt.month == DateTime.now().month,
+          )
+          .toList();
+
+      return CustomScrollView(
+        slivers: [
+          homeSliverAppbar(
+            onTapProfile: onTapProfile,
+            onTapNoti: onTapNoti,
+            feeAmount: feeAmount,
+            grossAmount: grossAmount,
+            netAmount: netAmount,
+            unreadNoti: unreadNoti,
           ),
-        ),
-        SliverList.builder(
-          itemCount: currentTransactions.length,
-          itemBuilder: (context, index) {
-            final transaction = currentTransactions[index];
-            return transactionRow(
-                transaction: transaction,
-                isLast: isLast(index, currentTransactions));
-          },
-        )
-      ],
-    );
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width08 * 2),
+                  child: summartBoxBelowAppBar(
+                      totalDepositNumber: totalDepositNumber,
+                      totalFreezingAmount: totalFreezingAmount),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: width08 * 2, vertical: height08),
+                  child: Text(
+                    'Current Month Transactions',
+                    style: textMd.copyWith(fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width08 * 2),
+                  child: tabPickerRow(
+                      onTapTab: (tabName) {
+                        setState(() {
+                          selectedTransactionType = tabName;
+                        });
+                      },
+                      tabNameList: tabNameList,
+                      selectedTab: selectedTransactionType),
+                ),
+                SizedBox(
+                  height: height08,
+                ),
+                if (currentTransactions.isEmpty)
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: height20),
+                    child: noTransactionsColumn,
+                  )
+              ],
+            ),
+          ),
+          SliverList.builder(
+            itemCount: currentTransactions.length,
+            itemBuilder: (context, index) {
+              final transaction = currentTransactions[index];
+              return transactionRow(
+                  transaction: transaction,
+                  isLast: isLast(index, currentTransactions));
+            },
+          )
+        ],
+      );
+    });
   }
 }
