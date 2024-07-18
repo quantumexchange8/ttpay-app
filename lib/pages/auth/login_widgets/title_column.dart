aeeec90e6c01e091353ccf8a26387a145bb075da
Zul <hanafirohani.2902@gmail.com>
2024-07-18 13:29:06 +0800

    change logo

--- lib/pages/auth/login_widgets/title_column.dart
+++ lib/pages/auth/login_widgets/title_column.dart
@@ -11,29 +11,29 @@ Column titleColumn(
     mainAxisSize: MainAxisSize.min,
     children: [
       Container(
-        width: height10 * 4.8,
-        height: height10 * 4.8,
+        width: width100 * 1.4,
+        height: height10 * 3.6,
         clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: const Alignment(0.71, -0.71),
-            end: const Alignment(-0.71, 0.71),
-            colors: [
-              Colors.white.withOpacity(0.1),
-              Colors.white.withOpacity(0.4)
-            ],
-          ),
+          // gradient: LinearGradient(
+          //   begin: const Alignment(0.71, -0.71),
+          //   end: const Alignment(-0.71, 0.71),
+          //   colors: [
+          //     Colors.white.withOpacity(0.1),
+          //     Colors.white.withOpacity(0.4)
+          //   ],
+          // ),
           image: DecorationImage(
               image: AssetImage(iconAddress), fit: BoxFit.cover),
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x4C000000),
-              blurRadius: 0.75,
-              offset: Offset(0.38, 0.56),
-              spreadRadius: 0.56,
-            )
-          ],
+          // borderRadius: BorderRadius.circular(12),
+          // boxShadow: const [
+          //   BoxShadow(
+          //     color: Color(0x4C000000),
+          //     blurRadius: 0.75,
+          //     offset: Offset(0.38, 0.56),
+          //     spreadRadius: 0.56,
+          //   )
+          // ],
         ),
       ),
       SizedBox(height: height24),
